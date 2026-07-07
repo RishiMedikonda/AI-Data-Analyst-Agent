e6e8cd0c2b66084e351b15803aab7ed328188de7
RishiMedikonda <medikonda0402@gmail.com_>
2026-07-07 22:25:34 +0530

    Added duplicate, datatype and date detection methods

--- agents/data_cleaning_agent.py
+++ agents/data_cleaning_agent.py
@@ -8,3 +8,31 @@ class DataCleaningAgent:
         missing = missing[missing > 0]
 
         return missing
+
+    @staticmethod
+    def detect_duplicate_rows(df):
+
+        return df.duplicated().sum()
+    
+    @staticmethod
+    def get_column_datatypes(df):
+
+        return df.dtypes
+    
+    @staticmethod
+    def detect_date_columns(df):
+
+        date_columns = []
+
+        for column in df.columns:
+
+            try:
+                pd.to_datetime(df[column])
+
+                date_columns.append(column)
+
+            except:
+
+                pass
+
+        return date_columns
