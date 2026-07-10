5bf212665f28726a0bc06b6fc22691f6d9d0ec1c
RishiMedikonda <medikonda0402@gmail.com_>
2026-07-10 22:05:11 +0530

    Added outlier detection using IQR

--- agents/data_cleaning_agent.py
+++ agents/data_cleaning_agent.py
@@ -1,5 +1,11 @@
+import pandas as pd
+from pandas.api.types import is_string_dtype
+import re
+
+ 
 class DataCleaningAgent:
 
+    #creating a method for detecting the missing values
     @staticmethod
     def detect_missing_values(df):
 
@@ -9,30 +15,124 @@ class DataCleaningAgent:
 
         return missing
 
+    #creating a method for detecting the duplicated rows
     @staticmethod
     def detect_duplicate_rows(df):
 
         return df.duplicated().sum()
     
+    #creating a method for retreiving column datatypes
     @staticmethod
     def get_column_datatypes(df):
 
-        return df.dtypes
+        return df.dtypes.astype(str)    
     
+    #creating a method for detecting the date columns
     @staticmethod
     def detect_date_columns(df):
 
         date_columns = []
+        print(df.dtypes)
 
         for column in df.columns:
+            print(column, df[column].dtype)
+        for column in df.columns:
 
-            try:
-                pd.to_datetime(df[column])
+            if is_string_dtype(df[column]):
 
-                date_columns.append(column)
+                converted = pd.to_datetime(df[column], errors="coerce")
 
-            except:
-
-                pass
+                if converted.notna().sum() > 0:
+                    date_columns.append(column)
 
         return date_columns
+    
+    #creating a method for converting the date columns
+    @staticmethod
+    def convert_date_columns(df, date_columns):
+        for column in date_columns:
+            df[column] = pd.to_datetime(df[column], errors="coerce")
+
+        return df
+    
+    #creating a method for filling the missing values
+    @staticmethod
+    def fill_missing_values(df):
+
+        df = df.copy()
+
+        # Numeric columns
+        for column in df.select_dtypes(include="number").columns:
+
+            if df[column].isnull().sum() > 0:
+                df[column] = df[column].fillna(df[column].median())
+
+        # Categorical columns
+        for column in df.select_dtypes(include=["object", "string"]).columns:
+
+            if df[column].isnull().sum() > 0:
+                df[column] = df[column].fillna(df[column].mode()[0])
+
+        return df
+
+    #creating a method for removing duplicate rows
+    @staticmethod
+    def remove_duplicate_rows(df):
+
+        before = len(df)
+
+        df = df.drop_duplicates()
+
+        after = len(df)
+
+        removed = before - after
+
+        return df, removed
+    
+    #standardizing the column names using regular expressions
+
+    @staticmethod
+    def standardize_column_names(df):
+
+        df = df.copy()
+
+        df.columns = [
+            re.sub(r'[^a-zA-Z0-9_]', '', column.strip().lower().replace(" ", "_"))
+            for column in df.columns
+        ]
+
+        return df
+    
+    #detecting the outliers
+
+    @staticmethod
+    def detect_outliers(df):
+
+        outlier_summary = []
+
+        numeric_columns = df.select_dtypes(include="number").columns
+
+        for column in numeric_columns:
+
+            q1 = df[column].quantile(0.25)
+            q3 = df[column].quantile(0.75)
+
+            iqr = q3 - q1
+
+            lower_bound = q1 - (1.5 * iqr)
+            upper_bound = q3 + (1.5 * iqr)
+
+            outlier_count = (
+                (df[column] < lower_bound) |
+                (df[column] > upper_bound)
+            ).sum()
+
+            if outlier_count > 0:
+                outlier_summary.append({
+                    "Column": column,
+                    "Outlier Count": int(outlier_count),
+                    "Lower Bound": round(lower_bound, 2),
+                    "Upper Bound": round(upper_bound, 2)
+                })
+
+        return pd.DataFrame(outlier_summary)
--- frontend/app.py
+++ frontend/app.py
@@ -6,8 +6,8 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(PROJECT_ROOT)
 
 from tools.file_reader import FileReader
-
 from tools.data_profiler import DataProfiler
+from agents.data_cleaning_agent import DataCleaningAgent
 
 st.set_page_config(
     page_title="AI Data Analyst Agent",
@@ -15,7 +15,8 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("📊 AI Data Analyst Agent")
+st.title("📊 AI Agent")
+#st.title("📊 AI Data Analyst Agent")
 
 uploaded_file = st.file_uploader(
     "Upload CSV File",
@@ -25,12 +26,17 @@ uploaded_file = st.file_uploader(
 if uploaded_file is not None:
 
     df = FileReader.read_csv(uploaded_file)
+    #st.write(df.dtypes)
+    # st.write(df["Date"].dtype)
+    # st.write(df["Date"].head())
+    # st.write(df["Date"].iloc[0])
+    # st.write(type(df["Date"].iloc[0]))
+
 
     st.success("CSV Uploaded Successfully ✅")
 
 #showing the summary of the table
     summary = DataProfiler.get_summary(df)
-
     st.subheader("📊 Dataset Summary")
 
     col1, col2, col3 = st.columns(3)
@@ -55,7 +61,104 @@ if uploaded_file is not None:
     with col6:
         st.metric("Categorical Columns", summary["Categorical Columns"])
 
+
+    missing = DataCleaningAgent.detect_missing_values(df)
+
+    duplicates = DataCleaningAgent.detect_duplicate_rows(df)
+
+    date_columns = DataCleaningAgent.detect_date_columns(df)
+
+    df = DataCleaningAgent.convert_date_columns(df, date_columns)
+
+    datatypes = DataCleaningAgent.get_column_datatypes(df)
+    df = DataCleaningAgent.fill_missing_values(df)
+
+    df, removed_duplicates = DataCleaningAgent.remove_duplicate_rows(df)
+
+    df = DataCleaningAgent.standardize_column_names(df)
+
+    outliers = DataCleaningAgent.detect_outliers(df)
+    
 # showing the preview of dataset
     st.subheader("Dataset Preview")
 
     st.dataframe(df.head())
+
+# data cleaning
+    st.subheader("🧹 Data Cleaning Report")
+    st.write("### Missing Values")
+
+    if len(missing) > 0:
+        st.dataframe(missing)
+    else:
+        st.success("No Missing Values Found ✅")
+
+    st.write("### Duplicate Rows")
+    st.metric("Duplicate Rows", duplicates)
+
+    st.write("### Column Data Types")
+    st.dataframe(datatypes)
+
+    st.write("### Detected Date Columns")
+
+    if date_columns:
+        for col in date_columns:
+            st.success(f"📅 {col}")
+    else:
+        st.info("No Date Columns Found")
+
+    st.write("### Duplicate Cleaning")
+
+    if removed_duplicates > 0:
+        st.success(f"Removed {removed_duplicates} duplicate rows ✅")
+    else:
+        st.success("No duplicate rows to remove ✅")
+
+    st.write("### Cleaned Dataset")
+
+    st.dataframe(df.head())
+
+#getting summary of Cleaned Data Set
+
+    summary = DataProfiler.get_summary(df)
+    st.subheader("📊 Cleaned Dataset Summary")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric("Rows", summary["Rows"])
+
+    with col2:
+        st.metric("Columns", summary["Columns"])
+
+    with col3:
+        st.metric("Missing Values", summary["Missing Values"])
+
+    col4, col5, col6 = st.columns(3)
+
+    with col4:
+        st.metric("Duplicate Rows", summary["Duplicate Rows"])
+
+    with col5:
+        st.metric("Numeric Columns", summary["Numeric Columns"])
+
+    with col6:
+        st.metric("Categorical Columns", summary["Categorical Columns"])
+
+    
+    #displaying outliers
+    
+    st.write("### Outlier Detection")
+
+    if not outliers.empty:
+        st.warning(
+            f"Outliers were detected in {len(outliers)} numeric column(s)."
+        )
+
+        st.dataframe(
+            outliers,
+            use_container_width=True,
+            hide_index=True
+        )
+    else:
+        st.success("No outliers detected ✅")
